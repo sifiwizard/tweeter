1f0ac8e51b5b663e1f14cc14ffa89f099a13d935
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-21 19:58:35 -0600

    Tweet box and hover mechanics

---
+++ public/scripts/hover.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#tweet").mouseover(function() {
+    $(this).css({"box-shadow" : "5px 10px"});
+    const icons = $(this).find("footer").children()[1];
+    $($(icons).children()).mouseover(function() {
+      $(this).css({"color" : "gold"});
+    });
+
+    $($(icons).children()).mouseout(function() {
+      $(this).css({"color" : "#545149"});
+    });
+  });
+
+  $("#tweet").mouseout(function() {
+    $(this).css({"box-shadow" : "none"});
+  });
+
+});
